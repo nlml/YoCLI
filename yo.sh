b7843a1fcb644818e7a2f1df0a47d9df716079c3
Liam Schoneveld <liam.sch@gmail.com>
2023-02-14 13:31:46 +0900

    comment yo.sh

--- yo.sh
+++ yo.sh
@@ -1,12 +1,14 @@
 export yodir="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 function yo() {
+    # Check if user provided the 'N' argument
     if [[ $1 == "N" ]];
     then
         N=$2
         str="${@:3}"
     else
         N=1
+        # Query is concatentation of all other arguments
         str="$*"
     fi
     python3 $yodir/yo.py --N=$N --query="""$str"""
